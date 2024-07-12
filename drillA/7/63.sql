e9d5fded18582413ad178c1f7d01dc29128f8a21
regitabish <shiba.taiga.lab@gmail.com>
2024-07-13 08:19:03 +0900

    m

--- drillA/7/63.sql
+++ drillA/7/63.sql
@@ -1,7 +1,11 @@
---
-select subtable.口座番号
+select subtable.口座番号,subtable.日付,subtable.maxin,subtable.maxout
 from (
-    select max(入金額),max(出金額) 
+    --
+    select 口座番号,max(入金額) as maxin,max(出金額) as maxout, sum(入金額) as sumin,sum(出金額) as sumout,日付
     from 取引
     where 口座番号='3104451'
+    group by 口座番号, 日付
+    
     ) as subtable
+
+where sumin is not null and sumout is not null
