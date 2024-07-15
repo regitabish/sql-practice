cbcdd8f7f6bcdd9389fc25a7bb2462b410919e32
regitabish <shiba.taiga.lab@gmail.com>
2024-07-15 13:09:11 +0900

    69

--- drillA/8/67.sql
+++ drillA/8/67.sql
@@ -1,25 +1,8 @@
-select
-    口座.口座番号,
-    case
-        when 口座.口座番号 not in (
-            select
-                口座番号
-            from
-                廃止口座
-        ) then 名義
-        else '解約済み'
-    end,
-    case
-        when 口座.口座番号 not in (
-            select
-                口座番号
-            from
-                廃止口座
-        ) then 残高
-        else 0
-    end
-from
-    口座
-    join 取引 on 口座.口座番号 = 取引.口座番号
-where
-    取引.日付 = '2020-03-01'
+select 口座.口座番号,名義,残高
+from 口座
+join 取引
+on 口座.口座番号=取引.口座番号
+where 取引.日付 = '2020-03-01'
+and (
+    口座.口座番号 not in (select 口座番号 from 廃止口座)
+)
